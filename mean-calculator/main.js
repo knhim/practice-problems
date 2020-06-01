be2f203527fb9d48efbed1e6e11d556a9af154d5
Kevin Nhim <nhimkap4@gmail.com>
2020-06-01 10:18:29 -0700

    completed mean-calculator exercise

---
+++ mean-calculator/main.js
@@ -0,0 +1,12 @@
+
+function meanCalculator (numbersArray) {
+  var sum = 0;
+  for (var i = 0; i < numbersArray.length; i++) {
+    sum += numbersArray[i];
+  }
+  var average = sum / (numbersArray.length);
+  return average;
+}
+console.log(meanCalculator([1, 2, 3, 4, 5]));
+console.log(meanCalculator([7, 3, 1, 5, 3, 2]));
+console.log(meanCalculator([9, 3, 6]));
