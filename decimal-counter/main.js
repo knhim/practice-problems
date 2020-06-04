c3a8ac9272bf6af3e04ed07e12e53fd09aa1db59
Kevin Nhim <nhimkap4@gmail.com>
2020-06-04 10:30:20 -0700

    updated decimal-counter

--- decimal-counter/main.js
+++ decimal-counter/main.js
@@ -2,7 +2,7 @@ function decimalPlaceCounter (stringNumber) {
   var counter = 0;
   for (var i = 0; i < stringNumber.length; i++){
     if (stringNumber[i] === '.') {
-      for (var j = 0; j < stringNumber[j]; j ++){
+      for (var j = i + 1; j < stringNumber.length; j++){
         counter++;
       }
     }
@@ -13,3 +13,4 @@ function decimalPlaceCounter (stringNumber) {
 console.log(decimalPlaceCounter("127.245"));
 console.log(decimalPlaceCounter("145"));
 console.log(decimalPlaceCounter("23.00"));
+console.log(decimalPlaceCounter('123.2454'));
