d759ba7bd7363089a238700358ae40325cc06ba7
Kevin Nhim <nhimkap4@gmail.com>
2020-06-08 10:23:41 -0700

    updated one more time

--- is-the-average-whole/main.js
+++ is-the-average-whole/main.js
@@ -4,13 +4,12 @@ function isTheAverageWhole(numbersArray) {
   for (var i = 0; i < numbersArray.length; i++) {
     sum += numbersArray[i];
   }
-  avg = (sum / numbersArray.length
+  avg = (sum / numbersArray.length)
   if (sum % numbersArray.length === 0) {
     return true;
   } else {
     return false;
   }
-
 }
 
 console.log('[1, 3, 4, 4]:', isTheAverageWhole([1, 3, 4, 4]));
