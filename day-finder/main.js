fe2a63f245cb53d05eb4bd9ce0a7bfcaf1cdc98b
Kevin <nhimkap4@gmail.com>
2020-06-23 10:23:29 -0700

    completed day-finder exercise

---
+++ day-finder/main.js
@@ -0,0 +1,43 @@
+/*
+- define a function named dayFinder
+- has a string as its parameter
+- string is in (MM/DD/YYYY) format
+-returns a string which will be the day of the week
+
+new Date()? to create
+Date.prototype.getDay() returns an integer, where 0 is Sunday
+*/
+
+function dayFinder(stringDate) {
+  let date = new Date(stringDate);
+  let number =  date.getDay();
+
+  switch(number) {
+    case 0:
+      console.log('Sunday');
+      break;
+    case 1:
+      console.log('Monday');
+      break;
+    case 2:
+      console.log('Tuesday');
+      break;
+    case 3:
+      console.log('Wednesday');
+      break;
+    case 4:
+      console.log('Thursday');
+      break;
+    case 5:
+      console.log('Friday');
+      break;
+    case 6:
+      console.log('Saturday');
+      break;
+  }
+}
+
+console.log(dayFinder("05/22/1946"));
+console.log(dayFinder("01/20/2001"));
+console.log(dayFinder("03/15/1988"));
+console.log(dayFinder("06/23/2020"));
