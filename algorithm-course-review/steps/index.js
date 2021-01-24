173e52d851069ba11d3968e0295889eeb1303873
Kevin Nhim <nhimkap4@gmail.com>
2021-01-24 04:56:42 -0800

    added another solution to steps problem

--- algorithm-course-review/steps/index.js
+++ algorithm-course-review/steps/index.js
@@ -18,6 +18,28 @@
 //       '####'
 
 //recursive solution
+function steps(n, row = 0, stair = '') {
+
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+
+}
+
+
 
 
 //solution with 2 for loops
