4414783cfe9d6f0d7a39fa4103976b239e4e6bc6
Kevin Nhim <nhimkap4@gmail.com>
2021-01-26 20:04:37 -0800

    recursive solution for pyramids problem

--- algorithm-course-review/pyramids/main.js
+++ algorithm-course-review/pyramids/main.js
@@ -1,15 +1,39 @@
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let rows = 0; rows < n; rows++) {
-    let level = '';
+// non recursive solution
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let rows = 0; rows < n; rows++) {
+//     let level = '';
 
-    for (let columns = 0; columns < n * 2 - 1; columns++) {
-      if (midpoint - rows <= columns && midpoint + rows >= columns) {
-        level += '#'
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
+//     for (let columns = 0; columns < n * 2 - 1; columns++) {
+//       if (midpoint - rows <= columns && midpoint + rows >= columns) {
+//         level += '#'
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+//recursive solution
+
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
   }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2*n-1)/2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' '
+  }
+
+  pyramid (n, row, level + add);
 }
