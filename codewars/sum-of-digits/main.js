3536374a7d959ca80a15b1f194af39d7592d902a
knhim <nhimkap4@gmail.com>
2021-07-13 21:49:10 -0700

    added solution to sum-of-digits

---
+++ codewars/sum-of-digits/main.js
@@ -0,0 +1,25 @@
+// Description:
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in
+// this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// recursive solution
+
+function digital_root(n) {
+  if(n < 10) {
+    return n;
+  }
+  n = n.toString().split("");
+  let result = 0;
+  for(let i = 0; i < n.length; i++) {
+    result += parseInt(n[i]);
+  }
+  return result < 10 ? result : digital_root(result);
+}
