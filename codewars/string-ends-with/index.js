dae09a964716d6e26fe1a8d7ae86b9891b1ae51f
knhim <nhimkap4@gmail.com>
2022-06-13 18:02:43 -0700

    added solution

---
+++ codewars/string-ends-with/index.js
@@ -0,0 +1,20 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending) {
+  let length = ending.length;
+
+  if (ending.length === 0) {
+    return true;
+  }
+
+  if (str.slice(-length) === ending) {
+    return true;
+  }
+
+  return false;
+}
