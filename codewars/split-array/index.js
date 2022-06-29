e608398abcbf67af1be0585279a432b1fdd9f250
knhim <nhimkap4@gmail.com>
2022-06-28 20:17:26 -0700

    added solution

---
+++ codewars/split-array/index.js
@@ -0,0 +1,7 @@
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+  return string.split(' ');
+}
