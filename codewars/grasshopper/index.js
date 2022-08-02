71221e602dd84587416a2124837dac4e35ed06d6
knhim <nhimkap4@gmail.com>
2022-08-01 20:43:02 -0700

    added another solution

--- codewars/grasshopper/index.js
+++ codewars/grasshopper/index.js
@@ -9,3 +9,13 @@
 function greet(name, owner) {
   return name === owner ? 'Hello boss' : 'Hello guest';
 }
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
